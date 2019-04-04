548ada44c77d8a1c018284e07f79ab4104cf5aad
Daniel Indlekofer <di3@gmx.net>
2019-04-04 20:30:37 +0200

    eslint

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ export default (fn, wait = 100, immediate = false) => {
   let timeout;
   return function() {
     const context = this,
-          args = arguments,
-          callNow = immediate && !timeout,
-          fnc = () => {
-            timeout = null;
-            if (!immediate) fn.apply(context, args);
-          };
+      args = arguments,
+      callNow = immediate && !timeout,
+      fnc = () => {
+        timeout = null;
+        if (!immediate) fn.apply(context, args);
+      };
     clearTimeout(timeout);
     timeout = setTimeout(fnc, wait);
     if (callNow) fn.apply(context, args);
-  }
-}
+  };
+};
